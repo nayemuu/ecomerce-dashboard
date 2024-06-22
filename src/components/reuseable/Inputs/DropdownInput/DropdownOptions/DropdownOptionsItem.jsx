7581bc73a7456348b387967eb90dd959b6cf3b74
Nayem-Hasan <nayeemhasanpr@gmail.com>
2024-06-23 01:43:59 +0600

    working

--- src/components/reuseable/Inputs/DropdownInput/DropdownOptions/DropdownOptionsItem.jsx
+++ src/components/reuseable/Inputs/DropdownInput/DropdownOptions/DropdownOptionsItem.jsx
@@ -3,7 +3,7 @@ function DropdownOptionsItem({ item, handleClick, value }) {
   //   console.log('isSelected = ', isSelected);
   return (
     <div
-      className={`block text-left py-3 text-base font-normal leading-[19.09px] hover:bg-gray-200 px-2 ${
+      className={`block text-left py-3 text-base font-normal leading-[19.09px] hover:bg-gray-200 px-2 cursor-pointer ${
         isSelected ? 'bg-[#833586]/10' : ''
       }`}
       onClick={() => handleClick(item)}
